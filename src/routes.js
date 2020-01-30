aab93a69e01384188c30af94156dda761c4d36b3
Guilherme Jeronimo <gjpnkflyd@gmail.com>
2020-01-30 19:22:45 -0300

    done

---
+++ src/routes.js
@@ -0,0 +1,112 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+const projects = [];
+
+// MIDDLEWARES
+
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+function logRequests(req, res, next) {
+  console.count('Número de requisições');
+
+  return next();
+}
+
+routes.use(logRequests);
+
+// PROJECTS
+
+routes.get('/', (req, res) => {
+  return res.json({
+    message: 'Bem-Vindo ao Desafio #01 by Guilherme Jeronimo',
+  });
+});
+
+routes.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+routes.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: [],
+  };
+
+  projects.push(project);
+
+  return res.json(projects);
+});
+
+routes.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+routes.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.json({ message: 'Projeto excluído com sucesso!' });
+});
+
+// TASKS
+
+routes.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+routes.put('/projects/:id/tasks/:task', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { task } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks[task] = title;
+
+  return res.json(project);
+});
+
+routes.delete('/projects/:id/tasks/:task', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { task } = req.params;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.splice(task, 1);
+
+  return res.json({ message: 'Tarefa excluída com sucesso!' });
+});
+
+export default routes;
